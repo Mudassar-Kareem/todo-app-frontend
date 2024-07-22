4c36fb78d1c902030e92fdbf21d6e4d1c34e1106
Mudassar <mudassarkareem916@gmail.com>
2024-07-22 12:59:59 +0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,22 +8,22 @@ const Home = () => {
   const [todo,setTodo] =useState([]);
   const [task,setTask] =useState([]);
   useEffect(()=>{
-    axios.get("http://localhost:3001/get")
+    axios.get("https://todo-app-server-wine.vercel.app/get")
     .then(result =>setTodo(result.data))
     .catch(err =>(console.log(err)));
   })
   const editHandle =(id) =>{
-    axios.put("http://localhost:3001/update/" +id)
+    axios.put("https://todo-app-server-wine.vercel.app/update/" +id)
     .then(result=>{location.reload()})
     .catch(err =>(console.log(err)));
   }
   const deleteHandle =(id) =>{
-    axios.delete("http://localhost:3001/delete/" +id)
+    axios.delete("https://todo-app-server-wine.vercel.app/delete/" +id)
     .then(result=>{location.reload()})
     .catch(err =>(console.log(err)));
   }
   const addButtonHandler = ()=>{
-    axios.post("http://localhost:3001/add", {task: task})
+    axios.post("https://todo-app-server-wine.vercel.app/add", {task: task})
     .then(result=>{location.reload()})
     .catch(err =>(console.log(err)));
   };
